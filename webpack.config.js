effac60422d6a076663b4bab87465e34d82a3543
ST-IlnurNasyrov <ilnur.nasyrov@sensortower.com>
2020-03-02 00:12:35 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+/* global __dirname, require, module*/
+
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+} else {
+  mode = 'development';
+}
+
+const config = {
+  mode: mode,
+  entry: {
+    'lib/index.js': './src/index.js',
+    'lib/docs/index.js': './src/docs/index.js',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    path: __dirname,
+    filename: '[name]',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;
